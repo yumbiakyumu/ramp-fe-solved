31f3c5b0d8352861fccba082dc48677023cefe5c
Boniface Kyumu <yumbiakyumu@gmail.com>
2024-04-27 14:40:55 +0300

    bug 4 fix

--- src/hooks/usePaginatedTransactions.ts
+++ src/hooks/usePaginatedTransactions.ts
@@ -17,8 +17,8 @@ export function usePaginatedTransactions(): PaginatedTransactionsResult {
       }
     )
 
-/* 
-Approach taken bug 4: The goal is to ensure that when new data is loaded, it is appended to the existing transactions without replacing them entirely, thus preventing the loss of initial transactions.
+/* bug 4 fix.
+Approach taken: The goal is to ensure that when new data is loaded, it is appended to the existing transactions without replacing them entirely, thus preventing the loss of initial transactions.
  The old code only checked if either the response or previousResponse was null before setting paginatedTransactions, which didn't handle the case where previousResponse is null but response is not null.
  The new code addresses this by explicitly checking if response is null and returning it if so. Then, it checks if previousResponse is null and returns response if so.
    Finally, if both response and previousResponse are not null, it concatenates the data from both responses and sets the nextPage accordingly.
