7ecdb4ece6c6f3a797d13a266c0bf0e1d7a09f78
Boniface Kyumu <yumbiakyumu@gmail.com>
2024-04-27 14:52:39 +0300

    bug 5 fix

--- src/hooks/useEmployees.ts
+++ src/hooks/useEmployees.ts
@@ -3,6 +3,17 @@ import { Employee } from "../utils/types"
 import { useCustomFetch } from "./useCustomFetch"
 import { EmployeeResult } from "./types"
 
+// bug 5 fix Approach taken:
+// Part 1:
+// The goal is to ensure that the employee filter stops showing "Loading employees.." as soon as the request for employees is succeeded, even if other data is still loading.
+// In the old code, there was no separate loading state for employees, so the loading state was tied to the overall loading state, which caused the issue.
+// The new code introduces a separate loading state, employeesLoading, to track the loading state specifically for employees.
+
+// Part 2:
+// Additionally, when new transactions are loaded, the employee filter should not show "Loading employees..." again, as employees are already loaded.
+// To address this, the new code ensures that the loading state for employees is set to true before fetching new employees data when the "View more" button is clicked,
+// and it sets the loading state to false after fetching, ensuring that the filter does not show "Loading employees..." again unnecessarily.
+
 
 export function useEmployees(): EmployeeResult {
   const { fetchWithCache } = useCustomFetch()
